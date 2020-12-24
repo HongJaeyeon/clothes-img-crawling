6edeefa165aa30a98038ee7b5d49f599a6a10150
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2020-12-24 12:29:19 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -32,26 +32,14 @@ for categories in range(1, 6):
     big = driver.find_elements_by_class_name('big')[0]
     big.find_elements_by_xpath('./li')[0].find_elements_by_xpath('./ul')[0].find_elements_by_xpath('./li')[categories].find_elements_by_xpath('./a')[0].click()
     driver.implicitly_wait(time_to_wait=3)
-    
-    first_page = True
 
     pages = driver.find_elements_by_css_selector('#pagingArea')[0].find_elements_by_xpath('./a')
-    
-    #first_page일 때는 first 버튼이 활성화 되지 않아 구분 필요
-    if (first_page == True):
-        start_page = 1 
-    else: 
-        start_page = 2
-    
     #한 카테고리 내 페이지
-    for page in range(start_page, len(pages)-1): #first,last,현재 page 제외
-        if(first_page == False) :
-            pages[page].click()
-            driver.implicitly_wait(time_to_wait=3)
-
+    for page in range(1, len(pages)): #first,last,현재 page 제외
         #한 페이지 내 상품
         dspGood_li = driver.find_element_by_id('dspGood').find_elements_by_xpath('./li')
-        for goods in range(0, len(dspGood_li)):
+        for goods in range(0, 1): #len(dspGood_li)
+            dspGood_li = driver.find_element_by_id('dspGood').find_elements_by_xpath('./li')
             dspGood_li[goods].find_elements_by_xpath('./a')[0].click()
             driver.implicitly_wait(time_to_wait=3)
 
@@ -78,10 +66,15 @@ for categories in range(1, 6):
                 #다음 상세 이미지 불러오기
                 driver.find_element_by_id('zoom').find_element_by_class_name('lSNext').click() #driver.find_elements_by_css_selector('#zoom .lSNext')[0].click()
                 driver.implicitly_wait(time_to_wait=3)
-                
-            first_page = False
 
             driver.back()
-
+            pages = driver.find_elements_by_css_selector('#pagingArea')[0].find_elements_by_xpath('./a')
+            
+            if (page != len(pages)-1) :
+                if(page>=2):
+                    page = page + 1
+                pages[page].click()
+                driver.implicitly_wait(time_to_wait=3)  
+            
 driver.close()
 driver.quit();
